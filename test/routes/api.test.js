02acb9b3a5baaa29bd0a9537296a2b1b66379519
Gerardo Rafael Cabrera Herrera <gecabrera@outlook.com>
2022-05-01 22:25:15 -0500

    Commit: Subir configuración del servidor web a Github

---
+++ test/routes/api.test.js
@@ -0,0 +1,56 @@
+const app = require("./../../lib/routes/api");
+const request = require("supertest");
+
+describe("Probar rutas del servidor", () => { 
+    test("Probar ruta principal", (done) => {
+        request(app)
+            .get("/")
+            .expect(200)
+            .expect((res) => {
+                expect((res.body).message).toBe("Visual Thinking Api welcome!");
+            })
+            .end((err) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+
+    test("Requerimiento 01: Habilitar un endpoint para consultar todos los estudiantes con todos sus campos", (done) => {
+        request(app)
+            .get("/estudiantes")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(51);
+            })
+            .end((err) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+
+    test("Requerimiento 02: Habilitar un endpoint para consultar los emails de todos los estudiantes que tengan certificacion", (done) => {
+        request(app)
+            .get("/estudiantes/emails/certificacion")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(29);
+            })
+            .end((err) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+
+    test("Requerimiento 03: Habilitar un endpoint para consultar todos los estudiantes que tengan credits mayor a 500", (done) => {
+        request(app)
+            .get("/v1/estudiantes/aprobados")
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.length).toBe(27);
+            })
+            .end((err) => {
+                if (err) return done(err);
+                return done();
+            });
+    });
+});
